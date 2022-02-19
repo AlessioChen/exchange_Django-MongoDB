a82b22a5e734008f478a9e172c45a41f534da5db
alessio <alessiochen98@gmail.com>
2022-02-19 22:07:53 +0100

    update price and btc_quantity validation

--- mainApp/forms.py
+++ mainApp/forms.py
@@ -1,7 +1,21 @@
 from .models import Order
 from django import forms
 
+
 class OrderCreationForm(forms.ModelForm):
-  class Meta:
-    model = Order
-    fields = ('price' , 'btc_quantity')
+
+    def clean_price(self):
+        price = self.cleaned_data['price']
+        if price < 0:
+            raise forms.ValidationError("Price can not be negative")
+        return price 
+
+    def clean_btc_quantity(self):
+        btc_quanty = self.cleaned_data['btc_quantity']
+        if btc_quanty < 0: 
+          raise forms.ValidationError("BTC quantity can not be negative")
+        return btc_quanty
+
+    class Meta:
+        model = Order
+        fields = ('price', 'btc_quantity')
